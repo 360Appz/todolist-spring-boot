7ec3edcd7027ae0d28bc134c99808e149645c625
360Appz <cookiesbyjy@gmail.com>
2024-08-14 19:19:04 +0800

    Remove commented code in security config

--- src/main/java/com/todolist/todolist/config/SecurityConfig.java
+++ src/main/java/com/todolist/todolist/config/SecurityConfig.java
@@ -1,24 +1,5 @@
 package com.todolist.todolist.config;
 
-//import com.todolist.todolist.security.JwtRequestFilter;
-//import com.todolist.todolist.services.security.MyUserDetailsService ;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//
-//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.todolist.todolist.security.JwtRequestFilter;
 import com.todolist.todolist.services.security.MyUserDetailsService;
