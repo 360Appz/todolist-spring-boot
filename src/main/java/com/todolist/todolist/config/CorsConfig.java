ba861fea3723596eed3c0f8f318f408ad31f89be
360Appz <cookiesbyjy@gmail.com>
2024-08-24 01:40:09 +0800

    Cors config

---
+++ src/main/java/com/todolist/todolist/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.todolist.todolist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+	
+	//CORS Config to allow frontend to communicate with backend
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allows CORS for all endpoints
+                        .allowedOrigins("http://localhost:3000") // Allows requests from this origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
